f5a3fd81d5f9a5a8537daccf7c9eb5d0020bcdc4
zhibirc <zhibirc@users.noreply.github.com>
2014-07-15 23:43:49 +0300

    Update corrector.js

--- corrector.js
+++ corrector.js
@@ -37,31 +37,35 @@ function corrector(textContainer, is_eng, event) {
         textContainer.value = textContainer.value
             // replace unnecessary whitespace sequences with one whitespace
             .replace(/[ \t]+/g, " ")
+            // remove whitespaces between dots
+            .replace(/\.\s\.\s\./g, "...")
             // replace sequence of more than three dots with three ones
             .replace(/(\.){4,}/g, "$1$1$1")
-            // replace sequence of more than three question or exclamation marks with three ones
-            .replace(/[!?]{4,}/g, function(str) { return str.slice(0, 3); })
             // remove whitespaces between question and exclamation marks
             .replace(/(\?|!) (?=\?|!)/g, "$1")
+            // replace sequence of more than three question or exclamation marks with three ones
+            .replace(/[!?]{4,}/g, function(str) { return str.slice(0, 3); })
             // replace hyphen with a dash
             .replace(/(\s)-(\s)/g, "$1—$2")
             // replace left-side double quotes
             .replace(/(^|\s)"/g, function(str, p1) { return is_eng ? p1 + "“" : p1 + "«"; })
             // replace right-side double quotes
             .replace(/"(\s|[-.,:;?!]|$)/g, function(str, p1) { return is_eng ? "”" + p1 : "»" + p1; })
-            // remove whitespaces between dots
-            .replace(/\.\s\.\s\./g, "...")
-            // adding single whitespace after comma, colon, semicolon and after each sentence before next one
-            .replace(/([.,:;])(?=[«“a-zа-яё\d])/gi, '$1 ')
+            // adding single whitespace after comma, colon and semicolon
+            .replace(/([,:;])(?=[^ ])/gi, '$1 ')
+            // adding a thin whitespaces in initials and reductions
+            .replace(/( [a-zа-яё]\.)\s?([a-zа-яё]\.)/gi, function(str, p1, p2) { return p1 + "\u2009" + p2; })
+            // adding whitespace before each sentence in the given text
+            .replace(/(\.)(?=[a-zа-яё][^.])/g, '$1 ')
             // adding single whitespace after single ore more question or exclamation marks before the next sentence
-            .replace(/([!?])(?![!?”» ])/g, '$1 ')
+            .replace(/([!?])(?=[^!?”» ])/g, '$1 ')
             // inserting real copyright symbol
-            .replace(/\([cс]\)/gi, "©")
+            .replace(/\([cс]\)/gi, "\u00A9")
             // inserting real trade mark symbol
-            .replace(/\(r\)/gi, "®")
+            .replace(/\(r\)/gi, "\u00AE")
             // capitalize first letter in the text
             .replace(/^([a-zа-яё])/, function(str, p1) { return p1.toUpperCase(); })
             // capitalize first letter in the sentence
-            .replace(/([.?!])(\s?)([a-zа-яё])/g, function(str, p1, p2, p3) { return p1 + p2 + p3.toUpperCase(); });
+            .replace(/([.?!])(\s?)([a-zа-яё])(?=[a-zа-яё])/g, function(str, p1, p2, p3) { return p1 + p2 + p3.toUpperCase(); });
     }
 }
